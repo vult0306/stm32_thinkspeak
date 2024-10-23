51e15f525e5fd80259b2445b65f74198193a9cb3
vult0306 <letuanvu91@gmail.com>
2024-10-23 18:51:13 +0700

    add uart task

--- main/esp_http_client_example.c
+++ main/esp_http_client_example.c
@@ -27,14 +27,38 @@
 #include "esp_system.h"
 
 #include "esp_http_client.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
 
-#define MAX_HTTP_RECV_BUFFER 512
-#define MAX_HTTP_OUTPUT_BUFFER 2048
+#define MAX_HTTP_RECV_BUFFER    512
+#define MAX_HTTP_OUTPUT_BUFFER  2048
+
+#define RX_BUF_SIZE             1024
+#define RXD_PIN                 (GPIO_NUM_5)
 
 static const char *TAG = "HTTP_CLIENT";
-char api_key[] = "replace with key";
+char api_key[] = "";
 
-static void http_test_task(void *pvParameters)
+double temperature = 1.2;
+double humidity = 2.3;
+
+
+void init_uart(void)
+{
+    const uart_config_t uart_config = {
+        .baud_rate = 115200,
+        .data_bits = UART_DATA_8_BITS,
+        .parity = UART_PARITY_DISABLE,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    uart_driver_install(UART_NUM_1, RX_BUF_SIZE * 2, 0, 0, NULL, 0);
+    uart_param_config(UART_NUM_1, &uart_config);
+    uart_set_pin(UART_NUM_1, UART_PIN_NO_CHANGE, RXD_PIN, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+}
+
+static void http_task(void *pvParameters)
 {
 	char thingspeak_url[] = "https://api.thingspeak.com";
 	char data[] = "/update?api_key=%s&field1=%.2f&field2=%.2f";
@@ -54,7 +78,7 @@ static void http_test_task(void *pvParameters)
 	{
 		vTaskDelay(10000 / portTICK_PERIOD_MS);
 		strcpy(post_data, "");
-		snprintf(post_data, sizeof(post_data), data, api_key, 1.2, 2.3);
+		snprintf(post_data, sizeof(post_data), data, api_key, temperature, humidity);
 		ESP_LOGI(TAG, "post = %s", post_data);
         esp_http_client_set_url(client, post_data);
 
@@ -85,6 +109,22 @@ static void http_test_task(void *pvParameters)
 
 }
 
+static void uart_task(void *arg)
+{
+    static const char *uart_task_TAG = "uart_task";
+    esp_log_level_set(uart_task_TAG, ESP_LOG_INFO);
+    uint8_t* data = (uint8_t*) malloc(RX_BUF_SIZE + 1);
+    while (1) {
+        const int rxBytes = uart_read_bytes(UART_NUM_1, data, RX_BUF_SIZE, 1000 / portTICK_PERIOD_MS);
+        if (rxBytes > 0) {
+            data[rxBytes] = 0;
+            ESP_LOGI(uart_task_TAG, "Read %d bytes: '%s'", rxBytes, data);
+            ESP_LOG_BUFFER_HEXDUMP(uart_task_TAG, data, rxBytes, ESP_LOG_INFO);
+        }
+    }
+    free(data);
+}
+
 void app_main(void)
 {
     esp_err_t ret = nvs_flash_init();
@@ -93,6 +133,9 @@ void app_main(void)
       ret = nvs_flash_init();
     }
     ESP_ERROR_CHECK(ret);
+
+    // init uart
+    init_uart();
 
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
@@ -104,9 +147,6 @@ void app_main(void)
     ESP_ERROR_CHECK(example_connect());
     ESP_LOGI(TAG, "Connected to AP, begin http example");
 
-#if CONFIG_IDF_TARGET_LINUX
-    http_test_task(NULL);
-#else
-    xTaskCreate(&http_test_task, "http_test_task", 8192, NULL, 5, NULL);
-#endif
+    xTaskCreate(&http_task, "http_task",    8192, NULL, 5, NULL);
+    xTaskCreate(&uart_task, "uart_task",    4096, NULL, 5, NULL);
 }
