2de83232f3214559e52b4da10125aa1ccc43cc33
vult0306 <letuanvu91@gmail.com>
2024-10-23 20:25:07 +0700

    remove generated eth and usb otg code

--- stm32/Core/Src/main.c
+++ stm32/Core/Src/main.c
@@ -18,9 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "eth.h"
 #include "usart.h"
-#include "usb_otg.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -89,9 +87,7 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_ETH_Init();
   MX_USART3_UART_Init();
-  MX_USB_OTG_FS_PCD_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
